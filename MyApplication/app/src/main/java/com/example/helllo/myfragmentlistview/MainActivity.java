1c4a06281a494596036c67c8d8b00681ba5321cd
030ii <lovelygeee2@gmail.com>
2017-05-19 21:34:28 +0900

    화면 회전 fragment 전환 처리

--- MyApplication/app/src/main/java/com/example/helllo/myfragmentlistview/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/helllo/myfragmentlistview/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.helllo.myfragmentlistview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.listFragment);
+        listFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.mainFragment);
         detailFragment = new DetailFragment();
     }
 
@@ -22,6 +24,32 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().replace(R.id.container, listFragment).commit();
         } else if (index == 1) {
             getSupportFragmentManager().beginTransaction().replace(R.id.container, detailFragment).commit();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setContentView(R.layout.activity_main);
+
+        if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){ // 세로 방향 시 처리
+            Toast.makeText(this, "세로", Toast.LENGTH_SHORT).show();
+
+            getSupportFragmentManager().beginTransaction().remove(listFragment).commit();
+            getSupportFragmentManager().beginTransaction().remove(detailFragment).commit();
+
+            listFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.mainFragment);
+            detailFragment = new DetailFragment();
+
+        } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){ // 가로 방향 전환 시 처리
+            Toast.makeText(this, "가로 ", Toast.LENGTH_SHORT).show();
+
+            getSupportFragmentManager().beginTransaction().remove(listFragment).commit();
+            getSupportFragmentManager().beginTransaction().remove(detailFragment).commit();
+
+            listFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.listFragment);
+            detailFragment = (DetailFragment) getSupportFragmentManager().findFragmentById(R.id.detailFragment);
+
         }
     }
 }
